0dda64522d65d294d64d73169d6aee7193b48523
Marcelo Cure <marcelo.cure@ilegra.com>
2016-11-20 12:01:43 -0200

    Using environment variable

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {};
-config.elasticUrl = 'http://paas:b9b100eb787398dda5fe02b6b05e805b@dori-us-east-1.searchly.com'
+config.elasticUrl = process.env.ELASTIC_URL
 config.eventsIndex = 'fuse-events',
 config.eventsType = 'fuse-event'
 module.exports = config;
